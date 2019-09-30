aa16927aa72818eabe1e17ad3e6801266306d398
Vasyl Bodnaruk <vacbka@gmail.com>
2019-09-30 12:42:18 +0300

    first commit

---
+++ treasureJavascript.js
@@ -0,0 +1,59 @@
+var getRandomNumber = function (size) {
+
+	return Math.floor(Math.random() * size);
+}
+
+
+var getDistance = function (event, target) {
+
+	var diffX = event.offsetX - target.x;
+	var diffY = event.offsetY - target.y;
+
+	return Math.sqrt((diffX*diffX) + (diffY*diffY));
+}
+
+
+var getDistanceHint = function (distance) {
+
+	if (distance < 10) {
+		return "Burn";
+	} else if (distance < 20) {
+		return "Very hot";
+	} else if (distance < 40) {
+		return "Hot";
+	} else if (distance < 80) {
+		return "Warm";
+	} else if (distance < 160) {
+		return "Cold";
+	} else if (distance < 320) {
+		return "Very cold";
+	} else {
+		return "Freeze";	
+	}
+}
+
+
+var width = 400;
+var height = 400;
+var clicks = 0;
+
+var target = {
+	x: getRandomNumber(width),
+	y: getRandomNumber(height)
+};
+
+
+$("#map").click(function (event) {
+
+	clicks++;
+	
+	var distance = getDistance(event,target);
+
+	var distanceHint = getDistanceHint(distance);
+
+	$("#distance").text(distanceHint);
+
+	if (distance < 8) {
+		alert("Treasure found! Clicks made: " + clicks);
+	}
+});
